f440cd4da4812b4ed1c297edd8dad2f0057d709b
Francesco Bianco <info.francescobianco@gmail.com>
2022-04-05 22:38:34 +0200

    Fix executable path

--- lib/utils.bash
+++ lib/utils.bash
@@ -60,7 +60,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert bpkg executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
