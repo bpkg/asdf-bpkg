8496663767ffae7e12a859af13d60b67c2b8771f
Francesco Bianco <info.francescobianco@gmail.com>
2022-04-05 22:39:26 +0200

    Review GitHub URL

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for bpkg.
 GH_REPO="https://github.com/bpkg/bpkg"
 TOOL_NAME="bpkg"
 TOOL_TEST="bpkg --version"
